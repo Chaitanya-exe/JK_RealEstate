f2d9119f693590fc9192d9d85f0eb830f6c1195f
Chaitany-exe <jeetkrishna3333@gmail.com>
2025-02-13 15:06:01 +0530

    route changes

--- app/api/estate/get/route.js
+++ app/api/estate/get/route.js
@@ -1,10 +1,16 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import { NextURL } from "next/dist/server/web/next-url";
 
 async function handler(req){
     const userClient = new PrismaClient();
     try {
-        
+        const url = new NextURL(req.url);
+        const params = url.searchParams.forEach((value, key)=>{
+            return {key: value}
+        });
+        console.log(params)
+        return NextResponse.json({success:true, data:"request received"});
     } catch (err) {
         console.log(`${err}`);
         return NextResponse.json({error: "some error occured", success:false},{status:500});
